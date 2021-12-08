9acfb9278e83f1ae09a370963efe57981369db25
adishiritwick <91323393+adishiritwick@users.noreply.github.com>
2021-12-08 21:54:23 +0530

    Update a1.ps1

--- a1.ps1
+++ a1.ps1
@@ -20,19 +20,3 @@ $Headers.Add("X-IDENTITY-HEADER", $env:IDENTITY_HEADER)
 $Headers.Add("Metadata", "True") 
 $accessToken = Invoke-RestMethod -Uri $url -Method 'GET' -Headers $Headers
 Write-Output $accessToken.access_token
-
-
-$resource= "?resource=https://management.azure.com/" 
-$url = $env:IDENTITY_ENDPOINT + $resource 
-$Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]" 
-$Headers.Add("X-IDENTITY-HEADER", $env:IDENTITY_HEADER) 
-$Headers.Add("Metadata", "True") 
-$accessToken = Invoke-RestMethod -Uri $url -Method 'GET' -Headers $Headers
-Write-Output $accessToken.access_token
-$resource= "?resource=https://management.azure.com/" 
-$url = $env:IDENTITY_ENDPOINT + $resource 
-$Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]" 
-$Headers.Add("X-IDENTITY-HEADER", $env:IDENTITY_HEADER) 
-$Headers.Add("Metadata", "True") 
-$accessToken = Invoke-RestMethod -Uri $url -Method 'GET' -Headers $Headers
-Write-Output $accessToken.access_token
